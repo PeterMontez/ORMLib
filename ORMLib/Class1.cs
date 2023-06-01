8a5739906c091e3e95ff8b2dfb986895cdd8e80e
PeterMontez <peterson.montez9@gmail.com>
2023-06-01 09:44:27 -0300

    first commit

---
+++ ORMLib/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ORMlib;
+public class Class1
+{
+
+}
---
+++ ORMLib/DataAnnotations/ForeignKeyAttribute.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ORMLib.DataAnnotations;
+
+// No C# 11.0 em diante poderemos usar Atributos Genéricos
+public class ForeignKeyAttribute : Attribute
+{
+    public Type ForeignTable { get; set; }
+    public ForeignKeyAttribute(Type foreignTable)
+    => this.ForeignTable = foreignTable;
+}
